15a6e67a132eed00e7c83c6551c0fdb1de39bced
Vladd Cantor <crimsonvirtuoso@vladdyhell.bbrouter>
2022-02-22 19:12:04 +0800

    add access control

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
     header: {
         'Content-type': 'aplication/json',
+        'Access-Control-Allow-Credentials': true,
     },
 });
