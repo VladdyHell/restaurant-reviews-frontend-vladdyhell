3de8a321205ed3afda266ac8c25b700d33050963
Vladd Cantor <crimsonvirtuoso@vladdyhell.bbrouter>
2022-02-24 11:42:05 +0800

    update

--- src/http-common.js
+++ src/http-common.js
@@ -4,6 +4,5 @@ export default axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     header: {
         'Content-type': 'aplication/json',
-        'Access-Control-Allow-Credentials': true,
     },
 });
